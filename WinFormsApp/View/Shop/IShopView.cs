a6cbf664472421cd1b4c43ee2406b4f868ebcfed
Oleh Protsun <olegprocun@gmail.com>
2025-12-05 17:29:23 +0100

    First attemp to add shop logic

---
+++ WinFormsApp/View/Shop/IShopView.cs
@@ -0,0 +1,35 @@
+﻿using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinFormsApp.View.Shedule
+{
+    public interface IShopView
+    {
+        int ShopId { get; set; }
+        string ShopName { get; set; }
+        string? ShopDescription { get; set; }
+        ICollection<ScheduleModel> Schedules { get; set; }
+
+        event Func<CancellationToken, Task>? SearchEvent;
+        event Func<CancellationToken, Task>? AddEvent;
+        event Func<CancellationToken, Task>? EditEvent;
+        event Func<CancellationToken, Task>? DeleteEvent;
+        event Func<CancellationToken, Task>? SaveEvent;
+        event Func<CancellationToken, Task>? CancelEvent;
+        event Func<CancellationToken, Task>? OpenProfileEvent;
+
+        void SetListBindingSource(BindingSource days);
+        void SwitchToEditMode();
+        void SwitchToListMode();
+        void ClearInputs();
+        void ShowInfo(string text);
+        void ShowError(string text);
+        bool Confirm(string text, string? caption = null);
+        void SetValidationErrors(IDictionary<string, string> errors);
+        void ClearValidationErrors();
+        void SetProfile(ShopModel model);
+        void SwitchToProfileMode();
+    }
+}
